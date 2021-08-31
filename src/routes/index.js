35e98b19c0b18245d685ae25ba3fcb41db7bd4fb
inu <iwlee.dev@gmail.com>
2021-08-31 09:26:27 +0900

    404 컴포넌트 추가
    [추가]
    - 404 NotFound 컴포넌트 추가
    - 라우터 연결

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Home from './Home.svelte'
 import About from './About.svelte'
 import Movie from './Movie.svelte'
+import NotFound from './NotFound.svelte'
 
 export default {
   '/': Home,
   '/movie/:id': Movie,
-  '/about': About
+  '/about': About,
+  '*': NotFound,
 }
