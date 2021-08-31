18a1d24c557c6209b6f72cba1002db5549578616
inu <iwlee.dev@gmail.com>
2021-08-31 09:26:58 +0900

    Fade In 이벤트 적용
    [추가]
    - Fade in 이벤트 추가
    - 스크롤 복구 기능 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script>
-  import Router from 'svelte-spa-router'
+  import { fade } from 'svelte/transition'
+  import Router, { location } from 'svelte-spa-router'
   import routes from '~/routes'
   import Header from '~/components/Header.svelte'
   import Footer from '~/components/Footer.svelte'
 </script>
 
 <Header />
-<Router {routes} />
+{#key $location}
+  <div
+    in:fade>
+    <Router {routes} restoreScrollState />
+  </div>
+{/key}
 <Footer />
 <style lang="scss">
 
